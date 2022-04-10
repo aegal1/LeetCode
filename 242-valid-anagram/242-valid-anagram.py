9a92fe31ae0692ffd174c0f8b615c9f6bcc482e5
aegal1 <76413569+aegal1@users.noreply.github.com>
2022-04-10 16:49:37 -0600

    Time: 70 ms (35.81%), Space: 14.6 MB (39.75%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        #First solution
+        return sorted(s) == sorted(t)
+        
+        #Second solution o(n)
+        # return Counter(s) == Counter(t)
+        
+#         #Third solution o(s+t)
+#         if len(s) != len(t):
+#             return False
+        
+#         countS, countT = {}, {}
+        
+#         for i in range(len(s)):
+#             # following code counts each occurence of letter in string s and t
+#             countS[s[i]] = countS.get(s[i], 0)
+#             countT[t[i]] = countS.get(t[i], 0)
+        
+#         for c in countS:
+#             if countS[c] != countT.get(c, 0):
+#                 return False
+        
+#         return True
+
+            
+        
