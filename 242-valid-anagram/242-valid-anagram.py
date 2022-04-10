722f1c12b9aa5849d8a2b923fdc027d194b247dd
aegal1 <76413569+aegal1@users.noreply.github.com>
2022-04-10 17:10:14 -0600

    Time: 52 ms (66.54%), Space: 13.7 MB (89.36%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -13,21 +13,21 @@ class Solution(object):
         return Counter(s) == Counter(t)
         
 #         #Third solution o(s+t)
-#         if len(s) != len(t):
-#             return False
+        if len(s) != len(t):
+            return False
         
-#         countS, countT = {}, {}
+        countS, countT = {}, {}
         
-#         for i in range(len(s)):
-#             # following code counts each occurence of letter in string s and t
-#             countS[s[i]] = countS.get(s[i], 0)
-#             countT[t[i]] = countS.get(t[i], 0)
+        for i in range(len(s)):
+            # following code counts each occurence of letter in string s and t
+            countS[s[i]] = countS.get(s[i], 0)
+            countT[t[i]] = countS.get(t[i], 0)
         
-#         for c in countS:
-#             if countS[c] != countT.get(c, 0):
-#                 return False
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
         
-#         return True
+        return True
 
             
         
