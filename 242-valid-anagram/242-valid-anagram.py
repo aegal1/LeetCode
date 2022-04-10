f1d3d290c46724a2ba1389c0b44d129b2e6d25a8
aegal1 <76413569+aegal1@users.noreply.github.com>
2022-04-10 17:18:07 -0600

    Update 242-valid-anagram.py

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -6,28 +6,11 @@ class Solution(object):
         :rtype: bool
         """
         
-        #First solution
-        # return sorted(s) == sorted(t)
+        #First solution o(1)
+        return sorted(s) == sorted(t)
         
         #Second solution o(n)
         return Counter(s) == Counter(t)
         
-#         #Third solution o(s+t)
-        if len(s) != len(t):
-            return False
-        
-        countS, countT = {}, {}
-        
-        for i in range(len(s)):
-            # following code counts each occurence of letter in string s and t
-            countS[s[i]] = countS.get(s[i], 0)
-            countT[t[i]] = countS.get(t[i], 0)
-        
-        for c in countS:
-            if countS[c] != countT.get(c, 0):
-                return False
-        
-        return True
-
             
         
