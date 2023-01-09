c2ee3b8be739b9814af3d16e81fc8bebaba48281
Abubakar Egal <76413569+aegal1@users.noreply.github.com>
2023-01-09 03:51:30 -0700

    Time: 112 ms (74.33%), Space: 19.9 MB (22.80%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,12 +3,9 @@ class Solution:
         ans = defaultdict(list)
         
         for s in strs:
-            count=[0] * 26
-            
+            count = [0] * 26
             for c in s:
                 count[ord(c) - ord("a")] +=1
             ans[tuple(count)].append(s)
-        
         return ans.values()
             
-    
